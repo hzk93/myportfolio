1256224631154fea66e5022d4caff3a0cf7e92c7
Hassan <hassanzk93@gmail.com>
2024-08-12 01:02:29 +0100

    Completed! Will ipdate projects.

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main
       className="relative bg-black-100 flex justify-center 
-    items-center flex-col overflow-hidden mx-auto sm:px-10"
+    items-center flex-col mx-auto sm:px-10 overflow-clip"
     >
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
@@ -21,6 +23,8 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
